68c51888fac6bcf125afa5728613bea9d2e2579e
Caio Quirino Medeiros <caio.quirino.medeiros@gmail.com>
2019-07-30 08:58:03 -0300

    Sending username as well

--- app/Controllers/Http/SessionController.js
+++ app/Controllers/Http/SessionController.js
@@ -3,13 +3,13 @@
 const User = use("App/Models/User");
 
 class SessionController {
-  async create({ request, auth }) {
+  async create({ request, response, auth }) {
     const { email, password } = request.all();
 
     const token = await auth.attempt(email, password);
-    const { id } = await User.findByOrFail("email", email);
+    const { id, username } = await User.findByOrFail("email", email);
 
-    return { ...token, id };
+    return response.status(200).send({ ...token, id, username });
   }
 }
 
